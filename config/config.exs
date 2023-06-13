37c4c86521458e808ac0780e2f8f03996fce55e4
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-06-13 16:51:29 +0300

    Added bg

--- config/config.exs
+++ config/config.exs
@@ -17,6 +17,17 @@ config :hot_plate, HotPlateWeb.Endpoint,
   pubsub_server: HotPlate.PubSub,
   live_view: [signing_salt: "fKOfBW/c"]
 
+config :tailwind,
+  version: "3.3.2",
+  default: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../assets", __DIR__)
+  ]
+
 # Configures the mailer
 #
 # By default it uses the "Local" adapter which stores the emails
--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :hot_plate, HotPlateWeb.Endpoint,
   secret_key_base: "T8bzPf+HSLG3hEi2NSvUXvxStFkV99LuQJk9EL0BaXY1t/xKNm0nmaYx7DMSriJb",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
